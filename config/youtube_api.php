c977e353a11b0313e436b184b74a82459ef49b94
Tilson Mateus <tilsonmat@gmail.com>
2023-05-20 22:29:24 +0100

    [fix] - edit config file

--- config/youtube_api.php
+++ config/youtube_api.php
@@ -2,10 +2,15 @@
 
 return [
 
-    'api_key' => env('API_YOUTUBE_KEY','kvjkfdjkfdfjvdk'),
+    'api_key' => env('API_YOUTUBE_KEY'),
 
-    'channel_id' => env('CHANNEL_ID','dhcghydgchfd'),
+    'channel_id' => env('CHANNEL_ID'),
+    
+    'uri' => [
+        'api_uri_search' => 'https://www.googleapis.com/youtube/v3/search',
 
-    'api_url_search' => 'https://www.googleapis.com/youtube/v3/search',
+        'api_uri_playlist' => 'https://www.googleapis.com/youtube/v3/playlists',
+    ]
+    
 
 ];
