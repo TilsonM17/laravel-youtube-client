22ee09659816f1eb0ada5c8ad11e64cf1583ecfd
Tilson Mateus <tilsonmat@gmail.com>
2023-05-22 16:03:14 +0100

    [LY-1] -  Adicionar LIstagem de Playlist

--- tests/Features/ClientTest.php
+++ tests/Features/ClientTest.php
@@ -5,27 +5,37 @@ namespace Tests\Features;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Http;
 use Tilson\YoutubeApi\Client;
+use Tilson\YoutubeApi\Enums\OrderBy;
 use Tilson\YoutubeApi\Services\YoutubeDataApi;
 
 class ClientTest extends \Orchestra\Testbench\TestCase
 {
-    private function setConfigurationForTests()
-    {
-        config()->set('youtube_api.api_key', env('API_YOUTUBE_KEY'));
-        config()->set('youtube_api.channel_id', env('CHANNEL_ID'));
-    }
+    use \Tests\Traits\Configuration;
+
     /** @test */
-    public function it_should_be_list_playlist()
+    public function it_should_be_send_request_for_list_videos()
     {
         $this->setConfigurationForTests();
+        Http::fake();
 
-        // Http::fake();
-        
-        // $client = new Client(new YoutubeDataApi());
+        $client = new Client(new YoutubeDataApi());
 
-        // $result = $client->listChannelVideos()->limit()->get();
-        
-        dd(config());
+        $client->listChannelVideos()->orderBy(OrderBy::ORDER_BY_TITLE)->limit(10)->get();
 
+        Http::assertSentCount(1);
+    }
+
+    /** @test */
+    //function it_should_be_send_request_for_list_playlists()
+    public function it_should_be_send_request_for_list_playlists()
+    {
+        $this->setConfigurationForTests();
+        Http::fake();
+
+        $client = new Client(new YoutubeDataApi());
+
+        $a = $client->listChannelPlaylist()->limit()->get();
+
+        Http::assertSentCount(1);
     }
 }
