ce558fdaf9112ff988b1d5f7ba71703aca4c5283
Tilson Mateus <tilsonmat@gmail.com>
2023-05-22 15:31:22 +0100

    [feat] - edit readme and remove namespace and i am not using

--- tests/ValidationTest.php
+++ tests/ValidationTest.php
@@ -2,11 +2,6 @@
 
 namespace Tests;
 
-use Illuminate\Support\Facades\App;
-use \Tilson\YoutubeApi\Client;
-use \Tilson\YoutubeApi\Enums\OrderBy;
-use Tilson\YoutubeApi\Providers\YoutubeApiServiceProvider;
-
 class ValidationTest extends \Orchestra\Testbench\TestCase
 {
 
